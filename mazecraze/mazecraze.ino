902d8f2e358e9299588ec320d8796bc97b836f6e
user <user@ubuntu.interconnection.pri>
2015-11-28 15:18:30 -0800

    we fixed bunch of bugs

--- mazecraze/mazecraze.ino
+++ mazecraze/mazecraze.ino
@@ -7,7 +7,7 @@ int walls_row=0;
 int old_walls_row=0;
 int the_wall[8] = {
   HIGH,HIGH,HIGH,HIGH,LOW,HIGH,HIGH,HIGH};
-  
+
 int columms[8] = {
   11,14,15,3,17,4,8,9};
 int rows[8] = {
@@ -21,7 +21,7 @@ int right_button=0;
 int last_right_button_state=1;
 int last_left_button_state=1;
 int loop_counting=0;
-int loop_limit=800;
+int loop_limit=100;
 
 int pixels[8][8];
 
@@ -37,7 +37,7 @@ void random_wall(){
   for (int i = 0; i <8; i++) { 
     the_wall[i] = HIGH;
   }
-  
+
   int random_number = random(8);
   the_wall[random_number] = LOW;
 }
@@ -45,7 +45,7 @@ void random_wall(){
 void play_tone() {
   int duration = 100;
   frequency += 10;
-  if (frequency > 20000) {
+  if (frequency > 2000) {
     frequency = 0;
   }
   tone(speaker_pin, frequency, duration);  
@@ -81,28 +81,72 @@ void move_wall() {
     walls_row = 0;
     random_wall();
   }
+  tone(speaker_pin, 100, 10);
 }
 
+
+// set pixel from pixels array based on physical orientation
+void set_pixel(int row, int col) {
+
+  int memory_row = 0;
+  int memory_column = row;
+
+  if (col==0){
+    memory_row=7;
+  }
+  if (col==1){
+    memory_row=6;
+  }
+  if (col==2){
+    memory_row=5;
+  }
+  if (col==3){
+    memory_row=4;
+  }
+  if (col==4){
+    memory_row=3;
+  }
+  if (col==5){
+    memory_row=2;
+  }
+  if (col==6){
+    memory_row=1;
+  }
+  if (col==7){
+    memory_row=0;
+  }
+
+  int on_or_off = pixels[memory_column][memory_row];
+
+  if (on_or_off==HIGH) {
+    digitalWrite(rows[row], LOW);
+//    digitalWrite(columms[col], HIGH); 
+  }
+  else {
+    digitalWrite(rows[row], HIGH);  
+//    digitalWrite(columms[col], LOW);
+  }
+//  old_row = row;
+}
+
+
 void refresh_pixels() {
+
   for (int col = 0; col < 8; col++) {
     digitalWrite(columms[col], HIGH);
-
-    int physical_column = 7;
+/*
+    // turn off all pixels
     for (int row = 0; row < 8; row++) {
-      int pixel = pixels[row][physical_column];
-      if (pixel==HIGH) {
-        digitalWrite(rows[row], LOW);
-      }
-      --physical_column;
-    }
-
-    for (int row = 0; row < 8; row++) {
-//      int pixel = pixels[row][col];
       digitalWrite(rows[row], HIGH);
     }
-
+*/
+    for (int row = 0; row < 8; row++) {
+      set_pixel(row, col);
+    }  
+    delay(1);
     digitalWrite(columms[col], LOW);
-  }
+  }    
+
 }
 
 void setup() {
@@ -126,6 +170,7 @@ void setup() {
   pinMode(speaker_pin, OUTPUT);     
 }
 
+
 void loop() {
   loop_counting = loop_counting + 1;
   // put your main code here, to run repeatedly:
@@ -134,7 +179,7 @@ void loop() {
   int left_button_pressed= digitalRead(left_button);
 
   if (right_button_pressed==0) {
-    if(last_right_button_state==1 || loop_counting==loop_limit){
+    if(last_right_button_state==1){
       //digitalWrite(columms[ships_possision],LOW);
 
       if(ships_possision==7){
@@ -147,7 +192,7 @@ void loop() {
   }
 
   if (left_button_pressed==0) {
-    if(last_left_button_state==1 || loop_counting==loop_limit){
+    if(last_left_button_state==1){
       //digitalWrite(columms[ships_possision],LOW);
 
       if(ships_possision==0){
@@ -163,12 +208,13 @@ void loop() {
 
   last_left_button_state = left_button_pressed;
   last_right_button_state = right_button_pressed;
-  if (loop_counting==loop_limit) {
-    loop_limit = loop_limit - 5;
+  if (loop_counting >= loop_limit) {
+    loop_limit = loop_limit - 1;
     loop_counting=0;
     move_wall();
     check_walls_hit();
   }
   refresh_pixels();
 }
+
 
